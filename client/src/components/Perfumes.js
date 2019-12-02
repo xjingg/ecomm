39ba45068eb8d4f5e8f56998a86013af6c4a21f4
xbloo <junecug@gmail.com>
2019-12-02 00:10:03 -0500

    Add Signup component

--- client/src/components/Perfumes.js
+++ client/src/components/Perfumes.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Strapi from "strapi-sdk-javascript/build/main";
-import { Box, Heading, Text, Image, Card, Button } from "gestalt";
+// prettier-ignore
+import { Box, Heading, Text, Image, Card, Button, Mask, IconButton } from "gestalt";
+import { calculatePrice, setCart, getCart } from "../utils";
+import { Link } from "react-router-dom";
 const apiUrl = process.env.API_URL || "http://localhost:1337";
 const strapi = new Strapi(apiUrl);
 
 class Perfumes extends React.Component {
   state = {
     perfumes: [],
-    brand: ""
+    brand: "",
+    cartItems: []
   };
 
   async componentDidMount() {
@@ -33,15 +37,41 @@ class Perfumes extends React.Component {
       });
       this.setState({
         perfumes: response.data.brand.perfumes,
-        brand: response.data.brand.name
+        brand: response.data.brand.name,
+        cartItems: getCart()
       });
     } catch (err) {
       console.error(err);
     }
   }
 
+  addToCart = brew => {
+    const alreadyInCart = this.state.cartItems.findIndex(
+      item => item._id === brew._id
+    );
+
+    if (alreadyInCart === -1) {
+      const updatedItems = this.state.cartItems.concat({
+        ...brew,
+        quantity: 1
+      });
+      this.setState({ cartItems: updatedItems }, () => setCart(updatedItems));
+    } else {
+      const updatedItems = [...this.state.cartItems];
+      updatedItems[alreadyInCart].quantity += 1;
+      this.setState({ cartItems: updatedItems }, () => setCart(updatedItems));
+    }
+  };
+
+  deleteItemFromCart = itemToDeleteId => {
+    const filteredItems = this.state.cartItems.filter(
+      item => item._id !== itemToDeleteId
+    );
+    this.setState({ cartItems: filteredItems }, () => setCart(filteredItems));
+  };
+
   render() {
-    const { brand, perfumes } = this.state;
+    const { brand, perfumes, cartItems } = this.state;
 
     return (
       <Box
@@ -49,6 +79,11 @@ class Perfumes extends React.Component {
         display="flex"
         justifyContent="center"
         alignItems="start"
+        dangerouslySetInlineStyle={{
+          __style: {
+            flexWrap: "wrap-reverse"
+          }
+        }}
       >
         {/* Perfumes Section */}
         <Box display="flex" direction="column" alignItems="center">
@@ -56,7 +91,7 @@ class Perfumes extends React.Component {
           <Box margin={2}>
             <Heading color="orchid">{brand}</Heading>
           </Box>
-          {/* Perfumes */}
+          {/* Perfumes*/}
           <Box
             dangerouslySetInlineStyle={{
               __style: {
@@ -69,8 +104,8 @@ class Perfumes extends React.Component {
             justifyContent="center"
             padding={4}
           >
-            {perfumes.map(perfume => (
-              <Box paddingY={4} margin={2} width={210} key={perfume._id}>
+            {perfumes.map(brew => (
+              <Box paddingY={4} margin={2} width={210} key={brew._id}>
                 <Card
                   image={
                     <Box height={250} width={200}>
@@ -79,7 +114,7 @@ class Perfumes extends React.Component {
                         alt="Brand"
                         naturalHeight={1}
                         naturalWidth={1}
-                        src={`${apiUrl}${perfume.image.url}`}
+                        src={`${apiUrl}${brew.image.url}`}
                       />
                     </Box>
                   }
@@ -92,14 +127,18 @@ class Perfumes extends React.Component {
                   >
                     <Box marginBottom={2}>
                       <Text bold size="xl">
-                        {perfume.name}
+                        {brew.name}
                       </Text>
                     </Box>
-                    <Text>{perfume.description}</Text>
-                    <Text color="orchid">${perfume.price}</Text>
+                    <Text>{brew.description}</Text>
+                    <Text color="orchid">${brew.price}</Text>
                     <Box marginTop={2}>
                       <Text bold size="xl">
-                        <Button color="blue" text="Add to Cart" />
+                        <Button
+                          onClick={() => this.addToCart(brew)}
+                          color="blue"
+                          text="Add to Cart"
+                        />
                       </Text>
                     </Box>
                   </Box>
@@ -107,6 +146,60 @@ class Perfumes extends React.Component {
               </Box>
             ))}
           </Box>
+        </Box>
+
+        {/* User Cart */}
+        <Box alignSelf="end" marginTop={2} marginLeft={8}>
+          <Mask shape="rounded" wash>
+            <Box
+              display="flex"
+              direction="column"
+              alignItems="center"
+              padding={2}
+            >
+              {/* User Cart Heading */}
+              <Heading align="center" size="sm">
+                Your Cart
+              </Heading>
+              <Text color="gray" italic>
+                {cartItems.length} items selected
+              </Text>
+
+              {/* Cart Items */}
+              {cartItems.map(item => (
+                <Box key={item._id} display="flex" alignItems="center">
+                  <Text>
+                    {item.name} x {item.quantity} - $
+                    {(item.quantity * item.price).toFixed(2)}
+                  </Text>
+                  <IconButton
+                    accessibilityLabel="Delete Item"
+                    icon="cancel"
+                    size="sm"
+                    iconColor="red"
+                    onClick={() => this.deleteItemFromCart(item._id)}
+                  />
+                </Box>
+              ))}
+
+              <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                direction="column"
+              >
+                <Box margin={2}>
+                  {cartItems.length === 0 && (
+                    <Text color="red">Please select some items</Text>
+                  )}
+                </Box>
+                <Text size="lg">Total: {calculatePrice(cartItems)}</Text>
+                <Text>
+                  <Link to="/checkout">Checkout</Link>
+                </Text>
+              </Box>
+            </Box>
+          </Mask>
         </Box>
       </Box>
     );
